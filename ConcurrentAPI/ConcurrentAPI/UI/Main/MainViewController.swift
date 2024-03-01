a1e8167decf76a998dfbf6f5fffee52020eb7bc6
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-03-01 11:54:42 +0100

    Working. Need to refactor code: Move names to Data Manager

--- ConcurrentAPI/ConcurrentAPI/UI/Main/MainViewController.swift
+++ ConcurrentAPI/ConcurrentAPI/UI/Main/MainViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     //MARK: - Properties
     private var viewModel: MainViewModel?
-    private var isLoading: Bool = true
+    private var isLoading: Bool = false
+    private var names: [String] = []
     
     //MARK: - IBOutlets
     @IBOutlet weak var tvListOfPersons: UITableView!
@@ -23,35 +24,48 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         showLoading()
         
+        configureTableView()
+        
         // Crear una instancia de MainViewModel y asignarla al viewModel
         let dataManager = MainViewDataManager(apiClient: MainViewApiClient())
         viewModel = MainViewModel(dataManager: dataManager)
         
-        // Llamar al método para obtener los nombres de los usuarios
-        viewModel?.getUsers { [weak self] users, error in
+        // Call the method to get user names
+        viewModel?.getUsers { [weak self] names, error in
             guard let self = self else { return }
             if let error = error {
                 print("Error fetching user names: \(error.localizedDescription)")
                 return
             }
             
-            if let users = users {
-                // Imprimir los nombres de los usuarios por consola
-                for name in users.names {
-                    print("User Name: \(name.name)")
+            if let users = names {
+                // Update names array
+                self.names = users.names.map { $0.name }
+                
+                // Reload table view data on the main thread
+                DispatchQueue.main.async {
+                    self.tvListOfPersons.reloadData()
                 }
             }
             
-            // Ocultar la vista de carga
+            // Hide loading view
             self.isLoading = false
             self.showLoading()
         }
     }
-
-
+    
+    
     // MARK: - Functions
     func set(viewModel: MainViewModel) {
         self.viewModel = viewModel
+    }
+    
+    func configureTableView() {
+        // Configure table view
+        tvListOfPersons.dataSource = self
+        tvListOfPersons.delegate = self
+        tvListOfPersons.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
     }
     
     func showLoading() {
@@ -62,5 +76,25 @@ class MainViewController: UIViewController {
             vwLoadingView.isHidden = true
             aiLoadingActivity.stopAnimating()
         }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Handle row selection if needed
     }
 }
